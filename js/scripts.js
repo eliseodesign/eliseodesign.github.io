ace2bef70d610c6bf24430c2747a6f3d6f7de90a
Eliseo Francisco Arévalo Espinoza <96401071+eliseodesign@users.noreply.github.com>
2022-01-20 10:06:54 -0600

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+const toggleTheme = document.getElementById('toggle-theme');
+const toggleIcon = document.getElementById('toggle-icon');
+const toggleText = document.getElementById('toggle-text')
+
+
+toggleTheme.addEventListener('click', ()=>{
+    document.body.classList.toggle('dark')
+    if(toggleIcon.src.includes('moon.svg')){
+        toggleIcon.src='assets/icons/sun.svg'
+        toggleText.textContent='Light Mode'
+    }else{
+        toggleIcon.src='assets/icons/moon.svg'
+        toggleText.textContent='Dark Mode'
+    }
+})
+
+
+
+//////////////////////////////////7 
+// color 
+const toggleColor = document.getElementById('toggle-colors')
+
+const rootStyles = document.documentElement.style;
+
+toggleColor.addEventListener('click', (e)=>{
+    rootStyles.setProperty('--primary-color', e.target.dataset.color)
+})
