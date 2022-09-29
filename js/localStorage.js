6d41ceab0b43bb026323f2287f1af4a159649b6c
Eliseo Francisco Arévalo Espinoza <96401071+eliseodesign@users.noreply.github.com>
2022-09-29 15:54:13 -0600

    add local storage to mode theme

---
+++ js/localStorage.js
@@ -0,0 +1,7 @@
+if (typeof(Storage) !== 'undefined') {
+    console.log("buena bro")
+  // Código cuando Storage es compatible
+} else {
+
+ alert("te recomiendo cambiar de navegador ")
+}
--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,16 @@ const toggleTheme = document.getElementById('toggle-theme');
 const toggleIcon = document.getElementById('toggle-icon');
 const toggleText = document.getElementById('toggle-text')
 
+const body = document.querySelector("body")
+console.log(body)
+
+load()
 
 toggleTheme.addEventListener('click', ()=>{
     document.body.classList.toggle('dark')
+    store(body.classList.contains('dark'));
+
+
     if(toggleIcon.src.includes('moon.svg')){
         toggleIcon.src='assets/icons/sun.svg'
         toggleText.textContent='Light Mode'
@@ -16,12 +23,54 @@ toggleTheme.addEventListener('click', ()=>{
 
 
 
+
+function store(value){
+    localStorage.setItem('dark', value);
+}
+
+
+function load(){
+    const darkmode = localStorage.getItem('dark');
+
+    if(!darkmode){
+        store('false');
+    }else if(darkmode == 'true'){
+        body.classList.add('dark');
+        toggleIcon.src='assets/icons/sun.svg'
+        toggleText.textContent='Light Mode'
+
+        toggleIcon.src='assets/icons/moon.svg'
+        toggleText.textContent='Dark Mode'
+        
+    }
+
+}
+
+
 //////////////////////////////////7 
 // color 
-const toggleColor = document.getElementById('toggle-colors')
+// loadColor()
+// const toggleColor = document.getElementById('toggle-colors')
 
-const rootStyles = document.documentElement.style;
+// const rootStyles = document.documentElement.style;
 
-toggleColor.addEventListener('click', (e)=>{
-    rootStyles.setProperty('--primary-color', e.target.dataset.color)
-})
+// toggleColor.addEventListener('click', (e)=>{
+//     rootStyles.setProperty('--primary-color', e.target.dataset.color);
+//     storeColor(e.target.dataset.color);
+    
+// })
+
+
+// function storeColor(value){
+//     alert(localStorage.setItem("color-actual", value))
+// }
+
+
+
+// function loadColor(){
+//     const color = localStorage.getItem("color-actual")
+//     if(color != undefined){
+
+//     }
+// }
+
