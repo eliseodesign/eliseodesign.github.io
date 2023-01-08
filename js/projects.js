f6ab9dc1fbcaec4c321175aa0ac178eb183575b3
Eliseo Arévalo <2480931@clases.edu.sv>
2023-01-08 12:09:12 -0600

    fixe

--- js/projects.js
+++ js/projects.js
@@ -1,19 +1,19 @@
 [
   {
-    "name": "Money manager",
-    "description": "A react app for personal budget",
-    "tecnologies": ["React", "Hooks", "localStorage"],
-    "demo": "https://react-money-manager.netlify.app/",
-    "github": "https://github.com/eliseodesign/react-money-manager",
-    "reference": "money-manager",
+    'name': 'Money manager',
+    'description': 'A react app for personal budget',
+    'tecnologies': ['React', 'Hooks', 'localStorage'],
+    'demo': 'https://react-money-manager.netlify.app/',
+    'github': 'https://github.com/eliseodesign/react-money-manager',
+    'reference': 'money-manager',
   },
   {
-    "name": "Pedro F. Cantor - web",
-    "description":
-      "Official website of the PEDRO F CANTOR school in which the institution publishes news and communications in addition to having its history, games and others",
-    "tecnologies": ["JS", "HTML", "Sass", "Responsive"],
-    "demo": "https://cantorweb.github.io/index.html",
-    "github": "https://github.com/eliseodesign/pedrofcantor",
-    "reference": "cantor",
+    'name': 'Pedro F. Cantor - web',
+    'description':
+      'Official website of the PEDRO F CANTOR school in which the institution publishes news and communications in addition to having its history, games and others',
+    'tecnologies': ['JS', 'HTML', 'Sass', 'Responsive'],
+    'demo': 'https://cantorweb.github.io/index.html',
+    'github': 'https://github.com/eliseodesign/pedrofcantor',
+    'reference': 'cantor',
   }
 ]
